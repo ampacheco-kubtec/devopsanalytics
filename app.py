50e3070fc2f32f30db2018920cfa6a090e024db9
ampacheco-kubtec <66849132+ampacheco-kubtec@users.noreply.github.com>
2022-04-16 16:31:26 -0400

    df load

--- app.py
+++ app.py
@@ -2,10 +2,34 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 #make a title for this demo using emojis
+
+#
+# Set of useful functions
+#
+
+#######################################################################################################################
+def read_csv(file_path):
+    df = pd.read_csv(file_path)
+    return df
+#######################################################################################################################
+
 st.title('DevOps Outlook')
 
+# APP connected to CognitiveService
+
+# First Panel
 st.title("Service Health")
 
+# Service Health Raw Data
+sh_rawdata="https://raw.githubusercontent.com/ampacheco-kubtec/devopsanalytics/main/data/services-health.csv"
+
+@st.cache
+df = read_csv(sh_rawdata)
+
+st.write(df)
+
+
+# Others Metric
 row2_1, row2_2 = st.columns([2,2])
 
 with row2_1:
