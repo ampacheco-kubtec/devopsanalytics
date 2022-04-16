37350407d711521fa8583f64098d30b875b5e07d
ampacheco-kubtec <66849132+ampacheco-kubtec@users.noreply.github.com>
2022-04-16 17:27:54 -0400

    chamullo fixed

--- app.py
+++ app.py
@@ -30,16 +30,6 @@ df = read_csv(sh_rawdata)
 st.write(df)
 
 st.map(df)
-row1_1, row1_2, row1_3 = st.columns(3)
-
-with row1_1:
-    st.title = "# Users per Service"
-
-with row1_2:
-    st.title = "# Users per Service"
-
-with row1_3:
-    st.title = "# Users per Service"
 
 
 # Others Metric
