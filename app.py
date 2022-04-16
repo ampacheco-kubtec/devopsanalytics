1c62a81beff1945e7660a6f92a4719a0c9ab2559
ampacheco <alejandro@alejandropacheco.net>
2022-04-16 19:33:23 +0000

    First Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+#make a title for this demo using emojis
+st.title('DevOps Outlook')
+
+st.title("Service Health")
+
+row2_1, row2_2 = st.columns([2,2])
+
+with row2_1:
+    st.title("Métrica 1")
+
+
+with row2_2:
+    st.title("Métrica 2")
+
+
+
+
